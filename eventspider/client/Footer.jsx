27100fdb026a846fe8879f1cc14d8a3ca9291954
Jacob Chapman <chapmajb@mail.uc.edu>
2015-12-16 18:47:35 -0500

    Navbar working

--- eventspider/client/Footer.jsx
+++ eventspider/client/Footer.jsx
@@ -1,7 +1,11 @@
 Footer = React.createClass({
+  navClick(event){
+    event.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  },
   render() {
-    return <nav class="navbar navbar-default navbar-fixed-bottom" id='footer' role="navigation">
-      <button type="button" class="btn btn-default navbar-btn glyphicon glyphicon-menu-hamburger" id="navBtn">
+    return <nav className="navbar navbar-default navbar-fixed-bottom" id='footer' role="navigation">
+      <button type="button" className="btn btn-default navbar-btn glyphicon glyphicon-menu-hamburger" id="navBtn" onClick={this.navClick}>
       </button>
     </nav>
   }
