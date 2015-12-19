31c6f0fb1f31f37db33cbe07a78531760a122ee2
Jacob Chapman <chapmajb@mail.uc.edu>
2015-12-18 23:54:53 -0500

    Mass React Update, along with correct publish -- subscribe

---
+++ eventspider/client/Events.jsx
@@ -0,0 +1,27 @@
+Feed = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    console.log('getting data');
+    return {
+      events: Events.find({}).fetch()
+    };
+  },
+
+  renderEvents() {
+    return this.data.events.map((event) => {
+      return <Event key={event._id} name={event.name} start_time={event.start_time} end_time={event.end_time} on_campus={event.on_campus}/>;
+    });
+  },
+  render() {
+    return (
+      <div className="events">
+        <ul>
+          {this.renderEvents()}
+        </ul>
+      </div>
+    );
+  }
+});
--- eventspider/client/routes.jsx
+++ eventspider/client/routes.jsx
@@ -37,6 +37,30 @@ Router.route('/register', function(){
   this.render('register');
 });
 */
+//Flow Helpers
+let pathFor = (path, params) => {
+  let query = params && params.query
+    ? FlowRouter._qs.parse(params.query)
+    : {};
+  return FlowRouter.path(path, params, query);
+};
+
+let urlFor = (path, params) => {
+  return Meteor.absoluteUrl(pathFor(path, params));
+};
+
+let currentRoute = (route) => {
+  FlowRouter.watchPathChange();
+  return FlowRouter.current().route.name === route
+    ? 'active'
+    : '';
+};
+
+FlowHelpers = {
+  pathFor: pathFor,
+  urlFor: urlFor,
+  currentRoute: currentRoute
+};
 
 // Flow Router here down
 FlowRouter.route('/', {
@@ -44,6 +68,19 @@ FlowRouter.route('/', {
     /* The key 'content' is now a function */
     ReactLayout.render(MainLayout, {
       content: <Login/>,
+      sidebar: <Nav/>,
+      footer: <Footer/>
+    });
+  }
+});
+
+FlowRouter.route('/feed', {
+  subscriptions: function() {
+    this.register('events', Meteor.subscribe('events'));
+  },
+  action: function(params) {
+    ReactLayout.render(MainLayout, {
+      content: <Feed/>,
       sidebar: <Nav/>,
       footer: <Footer/>
     });
