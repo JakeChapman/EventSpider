2b6b1937d4f9ee07fce8f6eca90ae65899558e18
Brendan Ryan <bren.ryan94@gmail.com>
2016-01-25 19:55:06 -0500

    Filter page css nearly complete

--- eventspider/client/Filter.jsx
+++ eventspider/client/Filter.jsx
@@ -4,19 +4,19 @@ Filter = React.createClass({
     return (
       <div className="filters">
         <div className="row">
-          <div id="filter-block">New</div>
-          <div id="filter-block">Popular</div>
-          <div id="filter-block">Friends</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />New</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Popular</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Friends</div>
         </div>
         <div className="row">
-          <div id="filter-block">Filter by Org.</div>
-          <div id="filter-block">Filter by Sub Category</div>
-          <div id="filter-block">Search</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Filter by Org.</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Filter by Sub Category</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Search</div>
         </div>
         <div className="row">
-          <div id="filter-block">Sports</div>
-          <div id="filter-block">General</div>
-          <div id="filter-block">Major</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Sports</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />General</div>
+          <div className="col-sm-4" id="filter-block"><img src="generic_profile.png" />Major</div>
         </div>
       </div>
     );
